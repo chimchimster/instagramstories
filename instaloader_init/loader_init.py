fc2b431ccdd9d59342a5cf6c144a01da247d89a2
chimchimster <jorddyishere@gmail.com>
2023-03-02 10:49:01 +0600

    added control rate limit functionallity

--- instaloader_init/loader_init.py
+++ instaloader_init/loader_init.py
@@ -1,9 +1,17 @@
-import os
 import time
 import random
 import instaloader
 
-loader = instaloader.Instaloader(storyitem_metadata_txt_pattern='', download_geotags=False)
+from instagramstories.logs.logger_init import LoggerWarn
+
+
+logger_warn = LoggerWarn()
+
+#
+loader = instaloader.Instaloader(rate_controller=lambda ctx: ControlRateLimit(ctx),
+                                 storyitem_metadata_txt_pattern='',
+                                 download_geotags=False,
+                                 )
 
 
 class SignIn:
@@ -21,12 +29,22 @@ class LoadStoriesOfUser:
         self.target = target
 
     def download_stories_of_target(self):
-        # Simulate human behaviour - wait from 3 to 10 seconds
-        time.sleep(random.randint(1,10))
-
+        # Apply to specific User
         profile = instaloader.Profile.from_username(loader.context, self.target)
 
-        time.sleep(random.randint(1, 10))
-
+        # Determine pattern where StoryItems have to be saved
         loader.dirname_pattern = f'/home/newuser/work_artem/instagramstories/media/{profile.username}/stories'
+
         loader.download_stories(userids=[profile.userid])
+
+
+class ControlRateLimit(instaloader.RateController):
+    def sleep(self, secs: float = 21):
+        raise logger_warn.logger.exception('There is a 429 error occurred')
+
+    def query_waittime(self, query_type, current_time, untracked_queries=False):
+        return 5 + super().query_waittime(query_type, current_time, untracked_queries)
+
+
+
+
--- instastoryparser/instaStoryParser.py
+++ instastoryparser/instaStoryParser.py
@@ -1,4 +1,3 @@
-import logging
 import os
 
 import instagramstories.instaloader_init.loader_init
@@ -6,8 +5,12 @@ import instagramstories.imagehandling.imagehandle
 import instagramstories.accounts.get_accs
 import instagramstories.db_init.database
 
+from instagramstories.logs.logger_init import LoggerWarn
+
 
 PATH_TO_ACCOUNTS = '/home/newuser/work_artem/instagramstories/accounts/account_for_parse.txt'
+logger_warning = LoggerWarn()
+
 
 def main():
     db = instagramstories.db_init.database.DataBase('stories')
@@ -33,13 +36,13 @@ def main():
     try:
         db.send_to_table('accounts', ('account',), instagramstories.accounts.get_accs.get_accounts(PATH_TO_ACCOUNTS))
     except Exception:
-        print('There is no accounts to send!')
+        logger_warning.logger.exception('There is no accounts to send!')
 
     # Get accounts for parse from database
     instagram_accounts = [account[0] for account in db.get_data_for_parse('account', 'accounts')]
 
     if not instagram_accounts:
-        raise Exception('There is no account to parse!')
+        raise logger_warning.logger.exception('There is no account to parse!')
 
     def login():
         try:
@@ -47,7 +50,7 @@ def main():
             signin = instagramstories.instaloader_init.loader_init.SignIn('manuelmathias92', 'iFppGcu4')
             signin.sign_in()
         except:
-            raise Exception('Account might be restricted')
+            raise logger_warning.logger.exception('Account might be restricted')
 
     collection_to_send = []
     def collect_data():
@@ -59,16 +62,16 @@ def main():
                 # Collect stories from account
                 user = instagramstories.instaloader_init.loader_init.LoadStoriesOfUser(account)
                 user.download_stories_of_target()
-            except:
-                print(f'There is an error while loading data from {account}')
+            except Exception:
+                logger_warning.logger.exception(f'There is an error while loading data from {account}')
 
             try:
                 # Trying to drag text from photos
                 os.chdir('/home/newuser/work_artem/instagramstories/media')
                 text_files = instagramstories.imagehandling.imagehandle.ImageHandling(os.getcwd() + directory_of_account)
                 text_files.create_txt_files()
-            except:
-                print(f'{account} has no text on photo to drag it')
+            except Exception:
+                logger_warning.logger.exception(f'{account} has no text on photo to drag it')
 
             try:
                 if account not in data_to_db:
@@ -80,8 +83,8 @@ def main():
                             data_to_db[account]['path_video'].append(os.getcwd() + directory_of_account + file)
                         elif file.endswith('.jpg'):
                             data_to_db[account]['path_photo'].append(os.getcwd() + directory_of_account + file)
-            except:
-                print('Account responded with status code 404')
+            except Exception:
+                logger_warning.logger.exception('Account responded with status code 404 or does not have StoryItems at all')
 
             for account, data in data_to_db.items():
                 account_id = db.get_account_id(account)
@@ -94,14 +97,11 @@ def main():
                         elif path == 'path_text':
                             collection_to_send.append([account_id, 3, element])
 
-    #login()
-    #collect_data()
+    login()
+    collect_data()
 
     # Migration
-    #db.send_to_table('attachments', ('account_id', 'type', 'path',), collection_to_send)
-
-
-
+    db.send_to_table('attachments', ('account_id', 'type', 'path',), collection_to_send)
 
 
 if __name__ == '__main__':
